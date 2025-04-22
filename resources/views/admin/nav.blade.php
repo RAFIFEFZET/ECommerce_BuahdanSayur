347008584f4373d228a07a5756921844f6c632e1
RAFIFEFZET <mrafiffawwaz040706@gmail.com>
2025-04-22 10:14:46 +0700

    icon v1.1

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -99,7 +99,7 @@
         <li class="nav-item">
             <a class="nav-link {{ request()->is('employees*') ? 'active' : '' }}" href="{{ route('employees.index') }}">
                 <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                    <i class="fa fa-user-o text-info text-sm opacity-10"></i>
+                    <i class="fas fa-users text-info text-sm opacity-10"></i>
                 </div>
                 <span class="nav-link-text ms-1">Employees</span>
             </a>
